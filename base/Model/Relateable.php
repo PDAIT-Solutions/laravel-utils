351f8746364318dc76bf4032eb155c3ffb47fd5c
Piotrek Zatorski <piotr.zat@gmail.com>
2019-10-14 16:48:02 +0200

    adde tag

--- base/Model/Relateable.php
+++ base/Model/Relateable.php
@@ -6,7 +6,7 @@ namespace PDAit\Base\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
-
+ 
 /**
  * Trait Relateable
  *
